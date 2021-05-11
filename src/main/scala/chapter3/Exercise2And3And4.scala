335d8575eb8ccbeb7aaf0327dcc43bf9d1b2b3a5
gaku <pro.gaku@gmail.com>
2021-05-12 06:03:06 +0900

    exercise4を実装

--- src/main/scala/chapter3/Exercise2And3And4.scala
+++ src/main/scala/chapter3/Exercise2And3And4.scala
@@ -1,6 +1,8 @@
 package com.gaku3601.studyscala
 package chapter3
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 
 case object Nil extends List[Nothing]
@@ -29,16 +31,26 @@ object List {
     case Cons(_, xs) => Cons(head, xs)
   }
 
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] =
+    if(n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, xs) => drop(xs, n-1)
+    }
+
   def apply[A](as: A*): List[A] =
     if(as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 }
 
-object Exercise2And3 {
+object Exercise2And3And4 {
   def main(args: Array[String]): Unit = {
     val list2 = List(1,2,3)
     println(List.tail(list2))
     val list3 = List(1,2,3)
     println(List.setHead(3, list3))
+    val list4 = List(1,2,3)
+    println(List.drop(list4, 1))
   }
 }
