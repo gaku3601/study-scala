0061ef150795e996910e9609948c1510576c928e
gaku <pro.gaku@gmail.com>
2021-05-14 05:56:25 +0900

    20実装

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -58,7 +58,11 @@ object List {
 
   def map[A,B](as: List[A])(f: A => B): List[B] = foldRight(as, Nil: List[B])((x, acc) => Cons(f(x), acc))
 
+  def concat[A](l: List[List[A]]): List[A] = foldRight(l, Nil:List[A])(append)
+
   def filter[A](as: List[A])(f: A => Boolean): List[A] = foldRight(as, Nil: List[A])((x, acc) => if(f(x)) Cons(x, acc) else acc)
+
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = concat(map(as)(f))
 
   def setHead[A](head: A,l: List[A]): List[A] = l match {
     case Nil => Nil
