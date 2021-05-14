7f21c79742948f0dd1ec2c2d4e2008d2a7b3366b
gaku <pro.gaku@gmail.com>
2021-05-15 07:39:35 +0900

    29実装

--- src/main/scala/chapter3/Tree.scala
+++ src/main/scala/chapter3/Tree.scala
@@ -28,4 +28,10 @@ object Tree {
     case Leaf(x) => Leaf(f(x))
     case Branch(l,r) => Branch(map(l)(f), map(r)(f))
   }
+
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
 }
