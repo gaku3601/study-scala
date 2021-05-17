62138f2daeb8753f1dea9cc9970eb2655aa7b38c
gaku <pro.gaku@gmail.com>
2021-05-18 06:03:40 +0900

    3実装

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -17,6 +17,11 @@ trait Stream[+A] {
     case Cons(_, t) if n > 0 => t().drop(n -1)
     case _ => this
   }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) =>  Stream.cons(h(), t() takeWhile p)
+    case _ => empty
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h:()=>A,t:()=>Stream[A]) extends Stream[A]
