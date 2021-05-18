533caee0fd902ba132247afbbf651a96b193904d
gaku <pro.gaku@gmail.com>
2021-05-19 07:09:25 +0900

    13実装

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -36,6 +36,40 @@ trait Stream[+A] {
   def filter(f: A => Boolean): Stream[A] = foldRight(empty)((h, t) => if(f(h)) Stream.cons(h, t) else t)
   def append[B >: A](s: Stream[B]): Stream[B] = foldRight(s)((h, t) => Stream.cons(h, t))
   def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((h, t) => f(h) append t)
+
+  //unfoldパターン
+  def mapViaUnfold[B](f: A => B): Stream[B] = Exercise11.unfold(this){
+    case Cons(h, t) => Some(f(h()), t())
+    case _ => None
+  }
+
+  def takeViaUnfold(n: Int): Stream[A] =
+    Exercise11.unfold((this,n)) {
+      case (Cons(h,t), 1) => Some((h(), (empty, 0)))
+      case (Cons(h,t), n) if n > 1 => Some((h(), (t(), n-1)))
+      case _ => None
+    }
+
+  def takeWhileViaUnfold(p: A => Boolean): Stream[A] = Exercise11.unfold(this){
+    case Cons(h, t) if p(h()) => Some(h(), t())
+    case _ => None
+  }
+
+  def zipWith[B,C](s2: Stream[B])(f: (A,B) => C): Stream[C] =
+    Exercise11.unfold((this, s2)) {
+      case (Cons(h1,t1), Cons(h2,t2)) => Some((f(h1(), h2()), (t1(), t2())))
+      case _ => None
+    }
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = zipWithAll(s2)((_,_))
+  def zipWithAll[B, C](s2: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] =
+    Exercise11.unfold((this, s2)) {
+      case (Empty, Empty) => None
+      case (Cons(h, t), Empty) => Some(f(Some(h()), Option.empty[B]) -> (t(), empty[B]))
+      case (Empty, Cons(h, t)) => Some(f(Option.empty[A], Some(h())) -> (empty[A] -> t()))
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(Some(h1()), Some(h2())) -> (t1() -> t2()))
+    }
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h:()=>A,t:()=>Stream[A]) extends Stream[A]
