ad79a9c70229034ba33506314c98811243c00e5f
gaku <pro.gaku@gmail.com>
2021-05-19 06:37:17 +0900

    12実装

--- src/main/scala/chapter5/Exercise11.scala
+++ src/main/scala/chapter5/Exercise11.scala
@@ -6,4 +6,14 @@ object Exercise11 {
     case Some((a, s)) => Stream.cons(a, unfold(s)(f))
     case None => Stream.empty
   }
+
+  val fibsViaUnfold: Stream[Int] =
+    unfold((0, 1)){ case (f0,f1) => Some((f0,(f1,f0+f1))) }
+    // or (f => Some((f._1,(f._2,f._1+f._2))))
+
+  def from(n: Int): Stream[Int] = unfold(n)(n => Some(n, n + 1))
+
+  def constant[A](a: A): Stream[A] = unfold(a)(_ => Some(a, a))
+
+  val onesViaUnfold: Stream[Int] = unfold(1)(_ => Some((1,1)))
 }
