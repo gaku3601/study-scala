d710ddda41f156116095a1afb2cec19dfd3c03d0
gaku <pro.gaku@gmail.com>
2021-05-21 06:20:06 +0900

    1実装

---
+++ src/main/scala/chapter6/Exercise1.scala
@@ -0,0 +1,9 @@
+package com.gaku3601.studyscala
+package chapter6
+
+object Exercise1 {
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+}
